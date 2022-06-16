a4f63955b2a506f7b67c719245e99d535d0681a5
jaklex <103463773+jaklex@users.noreply.github.com>
2022-06-15 21:16:48 -0400

    Delete M02 directory

--- M02/GPA Calculator.py
+++
@@ -1,7 +0,0 @@
-student = input("Please enter students full name: ")
-grade = float(input("Please enter students grade: "))
-
-if grade >= 3.5:
-    print(student, "is on the Dean's List.")
-elif grade < 3.5:
-    print(student, "is on the Honor Roll.")
